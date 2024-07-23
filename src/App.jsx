6c52fd0fe12737af05e519bf7d0171b3e0abc5fa
Jimmy DiColandrea <jdicolandrea@gmail.com>
2024-07-22 22:46:37 -0400

    forgot pillar component import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Pillar from "./Components/Pillar"
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -37,7 +39,7 @@ function App() {
           <Pillar placement="right"/>
         </div>
         <div id="header">
-          <img src={logoName} style={{width: "90%"}}/>
+          
         </div>
       </div>
     
